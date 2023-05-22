f6c606b5d10d69b27c391b60765fe7509526c82f
Aadam Kaivo <aadam.kaivo@glia.com>
2023-05-22 15:37:08 +0300

    Delete some stuff and create prod-us and prod-eu test sites.

---
+++ index.js
@@ -0,0 +1,63 @@
+
+const getGlia = () => {
+  return sm.getApi({version: 'v1'});
+}
+
+const setInfoValue = (elementId, value) => {
+  const element = document.getElementById(elementId);
+  element.innerText = value;
+};
+
+const copyOnClick = elementId => {
+  const element = document.getElementById(elementId);
+  if (element) {
+    element.addEventListener('click', () => {
+      navigator.clipboard.writeText(element.innerText);
+    });
+  }
+};
+
+const onUpdateInformation = customAttributesUpdateMethod => {
+  const customAttributes = JSON.parse(document.getElementById('updateInformation').value);
+  getGlia().then(glia => {
+    glia.updateInformation({customAttributes, customAttributesUpdateMethod});
+  });
+};
+
+const boot = () => {
+  copyOnClick('siteId');
+  copyOnClick('visitorId');
+  copyOnClick('engagementId');
+  copyOnClick('visitorCode');
+
+  getGlia().then(glia => {
+    setInfoValue('siteId', glia.getSiteId());
+    setInfoValue('visitorId', glia.getVisitorId());
+
+    const onEngagementStart = engagement => {
+      setInfoValue('engagementId', engagement.engagementId);
+    };
+
+    const onEngagementEnd = engagement => {
+      setInfoValue('engagementId', '');
+    };
+
+    glia.addEventListener(glia.EVENTS.ENGAGEMENT_START, onEngagementStart);
+    glia.addEventListener(glia.EVENTS.ENGAGEMENT_END, onEngagementEnd);
+
+    glia.omnibrowse.getVisitorCode().then(visitorCodeResponse => {
+      const element = document.getElementById('visitorCode');
+      element.innerText = visitorCodeResponse.code;
+    });
+  });
+};
+
+const externalSessionId = (new URLSearchParams(window.location.search)).get('external_session_id');
+
+window.getGliaContext = () => {
+  return {
+    sessionId: externalSessionId
+  };
+};
+
+window.addEventListener('load', boot);
